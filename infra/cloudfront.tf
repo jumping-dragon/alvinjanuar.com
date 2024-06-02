22d1d71129f6ed178dddb035092ab90373c8b38d
Alvin Januar <alvin.januar@ecloudvalley.com>
2024-06-03 03:30:18 +0800

    chore: add rerouting to posthog

--- infra/cloudfront.tf
+++ infra/cloudfront.tf
@@ -119,3 +119,187 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     minimum_protocol_version = "TLSv1.2_2021"
   }
 }
+
+resource "aws_cloudfront_distribution" "analytics_distribution" {
+  # Analytics Origin
+  origin {
+    domain_name = "us.i.posthog.com"
+    origin_id   = "analytics-origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  # Analytics assets Origin
+  origin {
+    domain_name = "us-assets.i.posthog.com"
+    origin_id   = "analytics-assets-origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "CDN for analytics of alvinjanuar.com"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "analytics-origin"
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "none"
+      }
+    }
+    function_association {
+      event_type   = "viewer-request"
+      function_arn = aws_cloudfront_function.redirector.arn
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  custom_error_response {
+    error_caching_min_ttl = 10
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }
+
+  custom_error_response {
+    error_caching_min_ttl = 10
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/404.html"
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/static/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = "analytics-assets-origin"
+
+    cache_policy_id = aws_cloudfront_cache_policy.analytics_cache_policy.id
+    origin_request_policy_id = aws_cloudfront_origin_request_policy.analytics_origin_request_policy.id
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.analytics_response_header_policy.id
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  # Cache behavior with precedence 1
+  ordered_cache_behavior {
+    path_pattern     = "*"
+    allowed_methods  = ["POST", "HEAD", "PATCH", "DELETE", "PUT", "GET", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "analytics-origin"
+
+    cache_policy_id = aws_cloudfront_cache_policy.analytics_cache_policy.id
+    origin_request_policy_id = aws_cloudfront_origin_request_policy.analytics_origin_request_policy.id
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.analytics_response_header_policy.id
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+        restriction_type = "none"
+    #   restriction_type = "whitelist"
+    #   locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+  
+  viewer_certificate {
+    cloudfront_default_certificate = true
+    # acm_certificate_arn = "${aws_acm_certificate.cert.arn}"
+    ssl_support_method = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+}
+
+resource "aws_cloudfront_cache_policy" "analytics_cache_policy" {
+  name        = "analytics-cache-policy"
+  comment     = "Analytics Cache policy"
+  default_ttl = 50
+  max_ttl     = 100
+  min_ttl     = 1
+  parameters_in_cache_key_and_forwarded_to_origin {
+    cookies_config {
+      cookie_behavior = "none"
+    }
+    headers_config {
+      header_behavior = "whitelist"
+      headers {
+        items = ["Origin", "Authorization"]
+      }
+    }
+    query_strings_config {
+      query_string_behavior = "all"
+    }
+  }
+}
+
+resource "aws_cloudfront_response_headers_policy" "analytics_response_header_policy" {
+  name    = "analytics-response-header-policy"
+  comment = "Analytics Response Header Policy"
+
+  cors_config {
+    access_control_allow_credentials = true
+
+    access_control_allow_headers {
+      items = ["Origin", "Authorization"]
+    }
+
+    access_control_allow_methods {
+      items = ["ALL"]
+    }
+
+    access_control_allow_origins {
+      items = ["www.alvinjanuar.com", "alvinjanuar.com"]
+    }
+
+    origin_override = false
+  }
+}
+
+resource "aws_cloudfront_origin_request_policy" "analytics_origin_request_policy" {
+  name    = "analytics-origin-request-policy"
+  comment = "Analytics Origin Request Policy"
+  cookies_config {
+    cookie_behavior = "none"
+  }
+  headers_config {
+    header_behavior = "whitelist"
+    headers {
+      items = ["Origin"]
+    }
+  }
+  query_strings_config {
+    query_string_behavior = "all"
+  }
+}
